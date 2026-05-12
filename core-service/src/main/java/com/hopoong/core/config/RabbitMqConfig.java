08ab42cdcdad58b860c7cb265b02be9a866d079c
ho_poong <hopoong23@gmail.com>
2026-05-12 22:28:22 +0900

    feat(core-service): RabbitMQ / 스케줄링 설정 추가

---
+++ core-service/src/main/java/com/hopoong/core/config/RabbitMqConfig.java
@@ -0,0 +1,31 @@
+package com.hopoong.core.config;
+
+import com.hopoong.core.keys.rabbitmq.RabbitMqKeys;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public TopicExchange userPointChangedExchange() {
+        return new TopicExchange(RabbitMqKeys.UserPointChanged.EXCHANGE, true, false);
+    }
+
+    @Bean
+    public MessageConverter rabbitMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter rabbitMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(rabbitMessageConverter);
+        return rabbitTemplate;
+    }
+}
