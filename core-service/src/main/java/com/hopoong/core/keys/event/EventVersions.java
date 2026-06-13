b95f274071a3acac01ddeda00a74cb8ae547d7e6
ho_poong <hopoong23@gmail.com>
2026-06-13 19:34:58 +0900

    feat(core-service): [key] UserPointChanged v2 MQ 계약 및 order v2 발행

--- core-service/src/main/java/com/hopoong/core/keys/event/EventVersions.java
+++ core-service/src/main/java/com/hopoong/core/keys/event/EventVersions.java
@@ -3,6 +3,7 @@ package com.hopoong.core.keys.event;
 public final class EventVersions {
 
     public static final String V1 = "v1";
+    public static final String V2 = "v2";
 
     private EventVersions() { }
 }
--- core-service/src/main/java/com/hopoong/core/keys/rabbitmq/RabbitMqKeys.java
+++ core-service/src/main/java/com/hopoong/core/keys/rabbitmq/RabbitMqKeys.java
@@ -7,9 +7,9 @@ public final class RabbitMqKeys {
 
     public static final class UserPointChanged {
 
-        public static final String EXCHANGE = "user.events.v1";
-        public static final String ROUTING_KEY = "user.point.changed";
-        public static final String QUEUE = "account-service.user.point.changed.v1";
+        public static final String EXCHANGE = "user.events.v2";
+        public static final String ROUTING_KEY = "user.point.changed.v2";
+        public static final String QUEUE = "processing-service.user.point.changed.v2";
         public static final String DLQ = QUEUE + ".dlq";
 
         private UserPointChanged() { }
